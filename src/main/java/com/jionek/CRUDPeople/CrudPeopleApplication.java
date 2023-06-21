9b5a6137cefe91178489aea77214a3c04430f3e7
Jionek95 <jakopiczko@wp.pl>
2023-06-21 13:48:34 +0200

    Initial commit

---
+++ src/main/java/com/jionek/CRUDPeople/CrudPeopleApplication.java
@@ -0,0 +1,13 @@
+package com.jionek.CRUDPeople;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudPeopleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudPeopleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jionek/CRUDPeople/CrudPeopleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jionek.CRUDPeople;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudPeopleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

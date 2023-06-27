c3053737f23c1aeecb6ed0e147327fb87956133f
Jionek95 <jakopiczko@wp.pl>
2023-06-27 19:51:07 +0200

    Make id parameter null for every Person obj in groupOfPeopleList

--- src/main/java/com/jionek/CRUDPeople/data/PersonDataLoader.java
+++ src/main/java/com/jionek/CRUDPeople/data/PersonDataLoader.java
@@ -19,13 +19,13 @@ public class PersonDataLoader implements ApplicationRunner {
     @Override
     public void run(ApplicationArguments args) throws Exception {
         List<Person> groupOfPeople = List.of(
-                new Person(10L, "Jake", "Snake", LocalDate.of(1950, 2, 24), new BigDecimal(20000)),
-                new Person(20L, "Sarah", "Smith", LocalDate.of(1960, 3, 23), new BigDecimal(30000)),
-                new Person(30L, "John", "Jackson", LocalDate.of(1970, 4, 22), new BigDecimal(40000)),
-                new Person(40L, "Bobby", "Black", LocalDate.of(1980, 5, 21), new BigDecimal(50000)),
-                new Person(50L, "Kim", "Knot", LocalDate.of(1990, 6, 20), new BigDecimal(60000)),
-                new Person(60L, "Johnny", "Jenkis", LocalDate.of(2000, 7, 19), new BigDecimal(70000)),
-                new Person(70L, "Merry", "McGuire", LocalDate.of(2001, 8, 18), new BigDecimal(80000))
+                new Person(null, "Jake", "Snake", LocalDate.of(1950, 2, 24), new BigDecimal(20000)),
+                new Person(null, "Sarah", "Smith", LocalDate.of(1960, 3, 23), new BigDecimal(30000)),
+                new Person(null, "John", "Jackson", LocalDate.of(1970, 4, 22), new BigDecimal(40000)),
+                new Person(null, "Bobby", "Black", LocalDate.of(1980, 5, 21), new BigDecimal(50000)),
+                new Person(null, "Kim", "Knot", LocalDate.of(1990, 6, 20), new BigDecimal(60000)),
+                new Person(null, "Johnny", "Jenkis", LocalDate.of(2000, 7, 19), new BigDecimal(70000)),
+                new Person(null, "Merry", "McGuire", LocalDate.of(2001, 8, 18), new BigDecimal(80000))
         );
         personRepository.saveAll(groupOfPeople);
     }

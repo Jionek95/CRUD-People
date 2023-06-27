ecda916d8ca383c40197a83ebd7be4b0a8964e71
Jionek95 <jakopiczko@wp.pl>
2023-06-27 19:40:37 +0200

    Create PersonRepository field and constructor with that field

--- src/main/java/com/jionek/CRUDPeople/data/PersonDataLoader.java
+++ src/main/java/com/jionek/CRUDPeople/data/PersonDataLoader.java
@@ -4,8 +4,14 @@ import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 
 public class PersonDataLoader implements ApplicationRunner {
+
+    private PersonRepository personRepository;
+
+    public PersonDataLoader(PersonRepository personRepository) {
+        this.personRepository = personRepository;
+    }
+
     @Override
     public void run(ApplicationArguments args) throws Exception {
-
     }
 }

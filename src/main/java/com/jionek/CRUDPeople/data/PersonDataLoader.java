086fd1858911dc09979f01d2e6e85e434d72082b
Jionek95 <jakopiczko@wp.pl>
2023-06-27 19:44:52 +0200

    1. paste List<Person> groupOfPeople to run() method 2. save that list to personRepository

--- src/main/java/com/jionek/CRUDPeople/data/PersonDataLoader.java
+++ src/main/java/com/jionek/CRUDPeople/data/PersonDataLoader.java
@@ -1,7 +1,12 @@
 package com.jionek.CRUDPeople.data;
 
+import com.jionek.CRUDPeople.business.model.Person;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
 
 public class PersonDataLoader implements ApplicationRunner {
 
@@ -13,5 +18,15 @@ public class PersonDataLoader implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
+        List<Person> groupOfPeople = List.of(
+                new Person(10L, "Jake", "Snake", LocalDate.of(1950, 2, 24), new BigDecimal(20000)),
+                new Person(20L, "Sarah", "Smith", LocalDate.of(1960, 3, 23), new BigDecimal(30000)),
+                new Person(30L, "John", "Jackson", LocalDate.of(1970, 4, 22), new BigDecimal(40000)),
+                new Person(40L, "Bobby", "Black", LocalDate.of(1980, 5, 21), new BigDecimal(50000)),
+                new Person(50L, "Kim", "Knot", LocalDate.of(1990, 6, 20), new BigDecimal(60000)),
+                new Person(60L, "Johnny", "Jenkis", LocalDate.of(2000, 7, 19), new BigDecimal(70000)),
+                new Person(70L, "Merry", "McGuire", LocalDate.of(2001, 8, 18), new BigDecimal(80000))
+        );
+        personRepository.saveAll(groupOfPeople);
     }
 }

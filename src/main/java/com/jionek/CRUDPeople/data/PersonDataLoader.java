66dff4109cfe568184e60f2cf9046854bcff4a21
Jionek95 <jakopiczko@wp.pl>
2023-06-27 19:35:02 +0200

    Create PersonDataLoader class that implements ApplicationRunner interface

---
+++ src/main/java/com/jionek/CRUDPeople/data/PersonDataLoader.java
@@ -0,0 +1,11 @@
+package com.jionek.CRUDPeople.data;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+
+public class PersonDataLoader implements ApplicationRunner {
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+
+    }
+}

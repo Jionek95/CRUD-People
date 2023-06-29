5df3e5902e2e52eee97139b80e1b96e8a838f574
Jionek95 <jakopiczko@wp.pl>
2023-06-29 15:02:39 +0200

    Annotate PersonDataLoader with @Component and change data (to be able to see if our change is implemented)

--- src/main/java/com/jionek/CRUDPeople/data/PersonDataLoader.java
+++ src/main/java/com/jionek/CRUDPeople/data/PersonDataLoader.java
@@ -3,11 +3,13 @@ package com.jionek.CRUDPeople.data;
 import com.jionek.CRUDPeople.business.model.Person;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.List;
 
+@Component
 public class PersonDataLoader implements ApplicationRunner {
 
     private PersonRepository personRepository;
@@ -20,7 +22,7 @@ public class PersonDataLoader implements ApplicationRunner {
     public void run(ApplicationArguments args) throws Exception {
         if (personRepository.count() == 0) {
             List<Person> groupOfPeople = List.of(
-                    new Person(null, "Jake", "Snake", LocalDate.of(1950, 2, 24), new BigDecimal(20000)),
+                    new Person(null, "Jake", "Joke", LocalDate.of(1950, 2, 24), new BigDecimal(20000)),
                     new Person(null, "Sarah", "Smith", LocalDate.of(1960, 3, 23), new BigDecimal(30000)),
                     new Person(null, "John", "Jackson", LocalDate.of(1970, 4, 22), new BigDecimal(40000)),
                     new Person(null, "Bobby", "Black", LocalDate.of(1980, 5, 21), new BigDecimal(50000)),

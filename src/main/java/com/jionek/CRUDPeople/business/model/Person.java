561a005bcc89170253f3dc2b5508247c57b3debd
Jionek95 <jakopiczko@wp.pl>
2023-07-21 19:00:32 +0200

    Add @NoArgsConstructor to Person class

--- src/main/java/com/jionek/CRUDPeople/business/model/Person.java
+++ src/main/java/com/jionek/CRUDPeople/business/model/Person.java
@@ -5,12 +5,14 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 @Entity
 public class Person {
 

02e426419ba48f64df12dcd93a211bd38df73926
Jionek95 <jakopiczko@wp.pl>
2023-07-21 19:45:45 +0200

    Clean imports

--- src/main/java/com/jionek/CRUDPeople/web/controller/PeopleController.java
+++ src/main/java/com/jionek/CRUDPeople/web/controller/PeopleController.java
@@ -3,7 +3,6 @@ package com.jionek.CRUDPeople.web.controller;
 import com.jionek.CRUDPeople.business.model.Person;
 import com.jionek.CRUDPeople.data.PersonRepository;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;

762bc4cd982ca4ed653fd0d10ffe63b22fe96b61
Jionek95 <jakopiczko@wp.pl>
2023-08-05 20:19:55 +0200

    Add @DecimalMin(value="10000") and @NotNull to salary field

--- src/main/java/com/jionek/CRUDPeople/business/model/Person.java
+++ src/main/java/com/jionek/CRUDPeople/business/model/Person.java
@@ -3,10 +3,7 @@ package com.jionek.CRUDPeople.business.model;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -26,13 +23,18 @@ public class Person {
 
     @NotEmpty(message = "First name cannot be empty")
     private String firstName;
+
     @NotEmpty(message = "Last name cannot be empty")
     private String lastName;
+
     @Past(message = "Date of birth must be in the past")
     @NotNull(message = "Date of birth cannot be null")
     private LocalDate dob;
 
+    @DecimalMin(value = "10000.00", message = "Salary must be at least 10000.00")
+    @NotNull(message = "Salary cannot be empty")
     private BigDecimal salary;
+
     @Email(message = "Email must be valid")
     @NotEmpty(message = "Email cannot be empty")
     private String email;

36066f1a375a99861f9b2fbffafe4cf2cc59fce2
Jionek95 <jakopiczko@wp.pl>
2023-08-09 11:30:04 +0200

    Comment old groupOfPeople List instead make it new ArrayList<>()

--- src/main/java/com/jionek/CRUDPeople/data/PersonDataLoader.java
+++ src/main/java/com/jionek/CRUDPeople/data/PersonDataLoader.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 //@Component
@@ -20,15 +21,17 @@ public class PersonDataLoader implements ApplicationRunner {
     @Override
     public void run(ApplicationArguments args) throws Exception {
         if (personRepository.count() == 0) {
-            List<Person> groupOfPeople = List.of(
-                    new Person(null, "Jake", "Joke", LocalDate.of(1950, 2, 24), new BigDecimal(20000), "jake@joke.com"),
-                    new Person(null, "Sarah", "Smith", LocalDate.of(1960, 3, 23), new BigDecimal(30000), "sarah@smith.com"),
-                    new Person(null, "John", "Jackson", LocalDate.of(1970, 4, 22), new BigDecimal(40000), "john@jackson.com"),
-                    new Person(null, "Bobby", "Black", LocalDate.of(1980, 5, 21), new BigDecimal(50000), "bobby@black.com"),
-                    new Person(null, "Kim", "Knot", LocalDate.of(1990, 6, 20), new BigDecimal(60000), "kim@knot.com"),
-                    new Person(null, "Johnny", "Jenkis", LocalDate.of(2000, 7, 19), new BigDecimal(70000), "johnny@jenkis.com"),
-                    new Person(null, "Merry", "McGuire", LocalDate.of(2001, 8, 18), new BigDecimal(80000), "merry@mcGuire.com")
-            );
+            List<Person> groupOfPeople =
+                      new ArrayList<>();
+//                    List.of(
+//                    new Person(null, "Jake", "Joke", LocalDate.of(1950, 2, 24), new BigDecimal(20000), "jake@joke.com"),
+//                    new Person(null, "Sarah", "Smith", LocalDate.of(1960, 3, 23), new BigDecimal(30000), "sarah@smith.com"),
+//                    new Person(null, "John", "Jackson", LocalDate.of(1970, 4, 22), new BigDecimal(40000), "john@jackson.com"),
+//                    new Person(null, "Bobby", "Black", LocalDate.of(1980, 5, 21), new BigDecimal(50000), "bobby@black.com"),
+//                    new Person(null, "Kim", "Knot", LocalDate.of(1990, 6, 20), new BigDecimal(60000), "kim@knot.com"),
+//                    new Person(null, "Johnny", "Jenkis", LocalDate.of(2000, 7, 19), new BigDecimal(70000), "johnny@jenkis.com"),
+//                    new Person(null, "Merry", "McGuire", LocalDate.of(2001, 8, 18), new BigDecimal(80000), "merry@mcGuire.com")
+//                    );
             personRepository.saveAll(groupOfPeople);
         }
     }

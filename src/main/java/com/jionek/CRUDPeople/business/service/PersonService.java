6484e4010b8849ce7275192f1c4ef599f18a08af
Jionek95 <jakopiczko@wp.pl>
2023-09-02 19:26:26 +0200

    Create deleteAllByID method in PersonService

--- src/main/java/com/jionek/CRUDPeople/business/service/PersonService.java
+++ src/main/java/com/jionek/CRUDPeople/business/service/PersonService.java
@@ -8,6 +8,10 @@ import org.springframework.stereotype.Service;
 
 import java.io.InputStream;
 import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 
 @Service
 public class PersonService {
@@ -35,7 +39,13 @@ public class PersonService {
         return personRepository.findAll();
     }
 
-    public void deleteAllById(Iterable<? extends Long> longs) {
-        personRepository.deleteAllById(longs);
+    public void deleteAllById(Iterable<Long> ids) {
+        Iterable<Person> peopleToDelete = personRepository.findAllById(ids);
+        Stream<Person> peopleStream = StreamSupport.stream(peopleToDelete.spliterator(), true);
+        Set<String> fileNames = peopleStream
+                .map(Person::getFileName)
+                .collect(Collectors.toSet());
+        personRepository.deleteAllById(ids);
+        storageRepository.deleteAllByNames(fileNames);
     }
 }

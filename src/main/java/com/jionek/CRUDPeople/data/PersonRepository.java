566ee84d1ec493caaaf0857ff22030ee8e5fbc1d
Jionek95 <jakopiczko@wp.pl>
2023-09-06 14:43:22 +0200

    Add @Query findFileNameByIds method to PersonRepository, that uses sql queries to select file names from db

--- src/main/java/com/jionek/CRUDPeople/data/PersonRepository.java
+++ src/main/java/com/jionek/CRUDPeople/data/PersonRepository.java
@@ -1,10 +1,17 @@
 package com.jionek.CRUDPeople.data;
 
 import com.jionek.CRUDPeople.business.model.Person;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Set;
 
 @Repository
 public interface PersonRepository extends CrudRepository<Person, Long> {
+
+    @Query(nativeQuery = true, value = "SELECT file_name FROM person WHERE id IN: ids")
+    public Set<String> findFileNameByIds(@Param("ids") Iterable<Long> ids);
 
 }

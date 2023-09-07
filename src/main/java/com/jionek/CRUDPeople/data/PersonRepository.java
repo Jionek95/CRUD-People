cfa30b6ec502e8a0242ea92453402ed3e718515e
Jionek95 <jakopiczko@wp.pl>
2023-09-07 11:37:03 +0200

    Make PersonRepository extend one interface backward that is PagingAndSortingRepository

--- src/main/java/com/jionek/CRUDPeople/data/PersonRepository.java
+++ src/main/java/com/jionek/CRUDPeople/data/PersonRepository.java
@@ -3,13 +3,14 @@ package com.jionek.CRUDPeople.data;
 import com.jionek.CRUDPeople.business.model.Person;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Set;
 
 @Repository
-public interface PersonRepository extends CrudRepository<Person, Long> {
+public interface PersonRepository extends PagingAndSortingRepository<Person, Long> {
 
     @Query(nativeQuery = true, value = "SELECT file_name FROM person WHERE id IN :ids")
     public Set<String> findFileNameByIds(@Param("ids") Iterable<Long> ids);

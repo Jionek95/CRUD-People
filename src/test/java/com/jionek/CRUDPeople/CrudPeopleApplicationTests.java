2a896aba36fca4b1314c02475403b404a81aa7b3
Jionek95 <jakopiczko@wp.pl>
2023-11-02 12:45:32 +0100

    Comment tests

--- src/test/java/com/jionek/CRUDPeople/CrudPeopleApplicationTests.java
+++ src/test/java/com/jionek/CRUDPeople/CrudPeopleApplicationTests.java
@@ -3,7 +3,7 @@ package com.jionek.CRUDPeople;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class CrudPeopleApplicationTests {
 
 	@Test

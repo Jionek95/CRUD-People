31886c2e424da14599b10121b486a38d667af2dd
Jionek95 <jakopiczko@wp.pl>
2023-11-06 16:15:41 +0100

    Comment created photoTry.html and PhotoController.java files

---
+++ src/main/java/com/jionek/CRUDPeople/web/controller/PhotoController.java
@@ -0,0 +1,47 @@
+//package com.jionek.CRUDPeople.web.controller;
+//
+//import com.jionek.CRUDPeople.business.model.Person;
+//import com.jionek.CRUDPeople.business.service.PersonService;
+//import com.jionek.CRUDPeople.data.FileStorageRepository;
+//import org.springframework.core.io.Resource;
+//import org.springframework.http.HttpHeaders;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.ui.Model;
+//import org.springframework.web.bind.annotation.*;
+//import lombok.extern.log4j.Log4j2;
+//
+//
+//import java.util.List;
+//import java.util.Optional;
+//
+//@Controller
+//@RequestMapping("/photo")
+//@Log4j2
+//public class PhotoController {
+//
+//    public static final String DISPO = """
+//             attachment; filename="%s"
+//            """;
+//    private FileStorageRepository fileStorageRepository;
+//    private PersonService personRepository;
+//
+//
+//    @GetMapping("/images/{resourcePath}")
+//    public ResponseEntity<Resource> getPhoto(@PathVariable String resourcePath){
+//        return ResponseEntity
+//                .ok()
+//                .header(HttpHeaders.CONTENT_DISPOSITION, String.format(DISPO, resourcePath))
+//                .body(fileStorageRepository.findByName(resourcePath));
+//    }
+//
+//    @PostMapping(params = "action=showWithPhoto")
+//    public String showPersonWithPhoto(@RequestParam("checkboxes") Optional<List<Long>> selections, Model model){
+//        log.info(selections);
+//        if (selections.isPresent()) {
+//            Optional<Person> personToShow = personRepository.findById(selections.get().get(0));     // for multiple chosen people option, we take first
+//            model.addAttribute("person", personToShow);
+//        }
+//        return "photo";
+//    }
+//}
